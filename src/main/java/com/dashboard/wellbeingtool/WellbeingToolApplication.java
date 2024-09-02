cd95ffee435aa14d58aa0310c36ea5e70d7cd24b
Sethu <sethuserge@gmail.com>
2024-09-02 21:23:17 +0200

    initial commit

---
+++ src/main/java/com/dashboard/wellbeingtool/WellbeingToolApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.wellbeingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboard/wellbeingtool/WellbeingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.wellbeingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
